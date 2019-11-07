8b0ab1653a37db511571f458ed1a8d47fdc30d03
XiaobingZhang <xiaobing.zhang@intel.com>
2019-11-07 22:59:34 +0800

    mkldnn backward: convolution, pooling, batchnorm, relu, linear, reshape
    Change-Id: I830c4db0e87cfa172abc0b107ce4146577977d10

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -1365,8 +1365,8 @@ def linear(input, weight, bias=None):
         - Bias: :math:`(out\_features)`
         - Output: :math:`(N, *, out\_features)`
     """
+
     if input.dim() == 2 and bias is not None:
-        # fused op is marginally faster
         ret = torch.addmm(bias, input, weight.t())
     else:
         output = input.matmul(weight.t())
@@ -1374,7 +1374,6 @@ def linear(input, weight, bias=None):
             output += bias
         ret = output
     return ret
-
 
 def bilinear(input1, input2, weight, bias=None):
     # type: (Tensor, Tensor, Tensor, Optional[Tensor]) -> Tensor
